235a081a27bf136f47d5d2073b16dcb4b42f7fa9
RamaMS06 <ramamuhammads08@gmail.com>
2022-05-19 10:05:00 +0700

    Wrap getting started

---
+++ Messanger/Controllers/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  Messanger
+//
+//  Created by Rama Muhammad S on 19/05/22.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Messanger/Controllers/NewConversationsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewConversationsViewController.swift
+//  Messanger
+//
+//  Created by Rama Muhammad S on 19/05/22.
+//
+
+import UIKit
+
+class NewConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
